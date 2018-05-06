ccb859ea47617ad0090d04a8d08bcd881c9c52ba
zhaochuanxing <zhaochuanxing@gmail.com>
2018-05-06 21:59:00 +0800

    init project

---
+++ 02-rendering-a-route/index.js
@@ -0,0 +1,15 @@
+import { render } from 'react-dom'
+import React from 'react'
+import App from './modules/App'
+import { Router, Route, hashHistory } from 'react-router'
+
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'))
